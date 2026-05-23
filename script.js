123ab6aa5b3495f8ebe55325ef72223c3ab26320
Heraij <brilliant2023.hotmail@gmail.com>
2026-05-23 20:47:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const demons = [
-  "TidalWave",
-  "Acheron"
+  {
+    name: "Tidal Wave",
+    video: "3v0w1qz"
+  },
+  {
+    name: "Acheron",
+    video: "dQw4w9WgXcQ"
+  }
 ];
 
 const grid = document.getElementById("demonGrid");
 
 if (grid) {
-  demons.forEach(name => {
+  demons.forEach(d => {
+
+    const thumbnail = `https://img.youtube.com/vi/${d.video}/maxresdefault.jpg`;
+
     grid.innerHTML += `
       <div class="demon">
-        <img src="images/${name}.png">
-        <p>${name}</p>
+        <img src="${thumbnail}">
+        <p>${d.name}</p>
       </div>
     `;
   });
