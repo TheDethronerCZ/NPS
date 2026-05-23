7d14ffce2525b5f880b0e95a11c345db33787183
Heraij <brilliant2023.hotmail@gmail.com>
2026-05-23 21:35:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const demons = [
   {
-    name: "Tidal Wave",
-    video: "3v0w1qz"
-  },
-  {
-    name: "Acheron",
-    video: "dQw4w9WgXcQ"
+    name: "TidalWave",
+    creator: "Unknown",
+    video: "dQw4w9WgXcQ",
+    id: "1001",
+    rank: 1
   }
 ];
 
@@ -13,14 +12,28 @@ const grid = document.getElementById("demonGrid");
 
 if (grid) {
   demons.forEach(d => {
-
-    const thumbnail = `https://img.youtube.com/vi/${d.video}/maxresdefault.jpg`;
-
     grid.innerHTML += `
-      <div class="demon">
-        <img src="${thumbnail}">
-        <p>${d.name}</p>
+      <div class="demon-card" onclick="openDemon('${d.name}')">
+        <img src="https://img.youtube.com/vi/${d.video}/maxresdefault.jpg">
+        <p>#${d.rank} ${d.name}</p>
       </div>
     `;
   });
 }
+
+function openDemon(name) {
+  const d = demons.find(x => x.name === name);
+  alert(
+    `Name: ${d.name}\nCreator: ${d.creator}\nID: ${d.id}\nRank: #${d.rank}`
+  );
+}
+
+/* LOADING SCREEN */
+window.onload = () => {
+  const loader = document.getElementById("loading");
+  if (!loader) return;
+
+  loader.style.display = "flex";
+  setTimeout(() => document.querySelector(".bar-fill").style.width = "100%", 100);
+  setTimeout(() => loader.style.display = "none", 1200);
+};
